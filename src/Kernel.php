042577d1c5e47305eeb4c72b8aaef8c70dbb4aa6
Aurimas Niekis <aurimas@niekis.lt>
2019-12-01 12:54:27 +0900

    Fixed bug

--- src/Kernel.php
+++ src/Kernel.php
@@ -51,11 +51,4 @@ class Kernel extends BaseKernel
         $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
     }
-
-    protected function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $this->process($container);
-    }
 }
